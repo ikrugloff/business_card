09e19f89c078f8e08ed91b1d3476211792c7353a
ikrugloff <i9200418888@gmail.com>
2018-09-23 14:44:52 +0300

    Added if - else construction and test for empty input list.

--- business_card/skills/views.py
+++ business_card/skills/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def skills_view(request):
-    return render(request, 'skills.html')
+    my_skills = ['Python 3+', 'Django 2+', 'Oracle SQL 11+', 'RedHat 7+']
+    # my_skills = []  # To test if - else construction in skills.html
+    return render(request, 'skills.html', {'my_skills': my_skills})
