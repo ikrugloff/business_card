7b11d0e96681b2362745c84a92262c464475d7ad
ikrugloff <i9200418888@gmail.com>
2018-09-24 18:07:34 +0300

    Context dictionary were added.

--- business_card/main_page/views.py
+++ business_card/main_page/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page_view(request):
-    return render(request, 'index.html')
+    f_name = 'Ilia'
+    l_name = 'Kruglov'
+    return render(request, 'index.html', {'first_name': f_name, 'last_name': l_name})  # Context dictionary
