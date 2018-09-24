b3c13a36f427a52eb2afb7759e71c662ca5be6fd
ikrugloff <i9200418888@gmail.com>
2018-09-24 22:59:43 +0300

    Finished "About" page.

--- business_card/about/views.py
+++ business_card/about/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def about_view(request):
-    return render(request, 'about.html')
+    f_name = 'Ilia'
+    age = '30'
+    return render(request, 'about.html',{'first_name': f_name, 'age': age})
