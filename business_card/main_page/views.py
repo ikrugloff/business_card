0e330a009ef73fb3c3acfa04788ea6bc85766f0f
ikrugloff <i9200418888@gmail.com>
2018-09-25 03:58:59 +0300

    Added tamplate filter "title".

--- business_card/main_page/views.py
+++ business_card/main_page/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page_view(request):
-    f_name = 'Ilia'
-    l_name = 'Kruglov'
+    f_name = 'ilia'
+    l_name = 'kruglov'
     return render(request, 'index.html', {'first_name': f_name, 'last_name': l_name})  # Context dictionary
