88ea5576ebdc8412a3b2484bef620ffa4e05c580
ikrugloff <i9200418888@gmail.com>
2018-09-29 01:55:20 +0300

    Added 'skills' table in DB (INSTALLED_APPS).

--- business_card/business_card/settings.py
+++ business_card/business_card/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'skills'  # Objects (my skills) will be saved in DB therefore they need a table
 ]
 
 MIDDLEWARE = [
