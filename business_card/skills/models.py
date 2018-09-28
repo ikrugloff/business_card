6b81af2fc63f672237ebd3baf066f0e9df7940a3
ikrugloff <i9200418888@gmail.com>
2018-09-29 01:54:02 +0300

    Added 2 models: SkillRank and Skill.

--- business_card/skills/models.py
+++ business_card/skills/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SkillRank(models.Model):
+    name = models.CharField(max_length=12, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=12, unique=True)
+    version = models.CharField(max_length=12, blank=True)
+    # 1 - * (одна ранг - много скилов)
+    rank = models.ForeignKey(SkillRank, on_delete=models.PROTECT, null=True)
+    # TODO: add a user
+
+    def __str__(self):
+        # return f'{self.name} {self.version} - Rank: {self.rank.name}'
+        return f'{self.name} {self.version}'
+
+"""
+>>> SkillRank.objects.all()
+<QuerySet [<SkillRank: Youngling>]>
+Как удалить один объект 'Youngling'???
+#############
+>>> SkillRank.objects.all()
+<QuerySet [<SkillRank: Youngling>, <SkillRank: Padawan>, <SkillRank: Knight>, <SkillRank: Master>, <SkillRank: Grand Master>]>
+Создался PK начиная с цифры 2, как начать нумерацию с начала, если первый раз ошибся со значением???
+
+"""
