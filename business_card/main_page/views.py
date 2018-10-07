26979c8bf08ad89782fc91824b1444811f09a906
ikrugloff <i9200418888@gmail.com>
2018-10-07 19:29:33 +0300

    The construction of changing the response parameters depending on the type of request has been added.

--- business_card/main_page/views.py
+++ business_card/main_page/views.py
@@ -6,10 +6,18 @@ def main_page_view(request):
     l_name = 'kruglov'
     print('Current user: ', request.user)
     print('Request type: ', request.method)
+    # print('Request parameters: ', params)
 
-
+    if request.method == 'GET':  # popular construction (in REST api more variants available) (29092019, 01:43:00)
+        params = request.GET
+        if 'login' in params:
+            login = params['login']
+    else:
+        params = request.POST
+        login = params['login']
 
     return render(request, 'index.html', {'first_name': f_name,
                                           'last_name': l_name,
-                                          'request_type': request.method
+                                          'request_type': request.method,
+                                          'params': params
                                           })  # Context dictionary
