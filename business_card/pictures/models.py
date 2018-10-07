e8884b79d50d21e22ad86c311b9ff44e86ed6876
ikrugloff <i9200418888@gmail.com>
2018-10-07 12:26:37 +0300

    Class "Picture" were added.

--- business_card/pictures/models.py
+++ business_card/pictures/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Picture(models.Model):
+    title = models.CharField(max_length=36, blank=False, null=False)  # null=False -> a title should be
+    photo = models.ImageField(blank=False, null=False)
+    text = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    # 58:50 29092018; null=True иначе не даст накатить
+
+    def __str__(self):
+        return f'{self.title}'
