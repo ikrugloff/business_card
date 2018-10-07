d7d453bb81ed804f262f0efee91eda08bd6110b4
ikrugloff <i9200418888@gmail.com>
2018-10-07 20:36:33 +0300

    user.views.py were updated with login_view.

--- business_card/user/views.py
+++ business_card/user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        # GET
+        # нарисовать страницу где можно ввести данные
+        return render(request, 'login.html')
