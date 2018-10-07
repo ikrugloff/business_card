208b0692adc504b5465a554501ccca6db39eb60a
ikrugloff <i9200418888@gmail.com>
2018-10-07 22:00:08 +0300

    logout_view were added.

--- business_card/user/views.py
+++ business_card/user/views.py
@@ -1,10 +1,25 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+def logout_view(request):
+    auth.logout(request)
+    return HttpResponseRedirect("/login/")
+
+
 def login_view(request):
     if request.method == 'POST':
-        pass
+        # получить данные
+        data = request.POST
+        # login = data['login']
+        login = data.get('login')  # Better get data with  method 'get'
+        password = data.get('password')
+        user = auth.authenticate(username=login, password=password)
+        if user:
+            auth.login(request, user)
+            return HttpResponseRedirect("/")
     else:
         # GET
         # нарисовать страницу где можно ввести данные
