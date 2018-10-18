f9a63e9509e7ab5032cfffcb773e74baab182532
ikrugloff <i9200418888@gmail.com>
2018-10-18 18:55:14 +0300

    Finished with cls "Picture" and with "pictures_model_view".

--- business_card/pictures/models.py
+++ business_card/pictures/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Picture(models.Model):
-    title = models.CharField(max_length=36, blank=False, null=False)  # null=False -> a title should be
-    photo = models.ImageField(blank=False, null=False)
-    text = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=36, blank=False, null=False)  # null=False -> a title should be
+    description = models.TextField(blank=True, null=True)  # unique=True if needed
+    start_date = models.DateTimeField(auto_now=True)
+    img = models.ImageField(upload_to='pictures', blank=False)  # How to avoid issues if already has a string w/o img?
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     # 58:50 29092018; null=True иначе не даст накатить
 
     def __str__(self):
-        return f'{self.title}'
+        return f'{self.name}'
--- business_card/pictures/views.py
+++ business_card/pictures/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Picture
 
 
 # Create your views here.
 def pictures_view(request):
     return render(request, 'pictures.html')
+
+
+def pictures_model_view(request):
+    pictures = Picture.objects.all()
+    return render(request, 'picture.html', {'objects': pictures})
